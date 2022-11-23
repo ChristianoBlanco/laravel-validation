c100e5e3066dbed2968570e9af7161956d8f859a
ChristianoBlanco <christiano.blanco@hotmail.com>
2022-11-23 15:58:57 -0300

    atualizacao helpers 23-11-2022

--- app/Http/Controllers/helperController.php
+++ app/Http/Controllers/helperController.php
@@ -1,11 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+
+use geekcom\ValidatorDocs\Rules\Cpf;
+use geekcom\ValidatorDocs\Rules\Cnpj;
+use geekcom\ValidatorDocs\Rules\Cnh;
+use geekcom\ValidatorDocs\Rules\Renavam;
+use geekcom\ValidatorDocs\Rules\Placa;
+use geekcom\ValidatorDocs\Rules\Ddd;
+use geekcom\ValidatorDocs\Rules\InscricaoEstadual;
+use geekcom\ValidatorDocs\Rules\Nis;
+use geekcom\ValidatorDocs\Rules\Cns;
+use geekcom\ValidatorDocs\Rules\Certidao;
+use geekcom\ValidatorDocs\Rules\TituloEleitoral;
 
 class helperController extends Controller
 {
+
+
     //
     public static function form(){
 
@@ -15,12 +28,37 @@ class helperController extends Controller
 
     public static function cpf(Request $request){
 
-       $rules =  [
-            'cpf' => 'required|cpf',
+        $rules =  [
+            #'cpf' => 'required|Cpf',
+            #'cnpj' => 'required|Cnpj',
+            #'cpfcnpj' => 'required|cpf_cnpj',
+            #'cnh' => 'required|Cnh',
+            #'renavam' => 'required|Renavam',
+            #'placa' => 'required|Placa',
+            #'inscricao_estadual' => 'required|inscricao_estadual:RJ',
+            #'nis' => 'required|nis',
+            #'cns' => 'required|Cns',
+            #'certidao' => 'required|Certidao',
+            #'ddd' => 'required|Ddd',
+            #'titulo' => 'required|titulo_eleitor',
         ];
+
         $message = [
-            'cpf.cpf' => 'CPF Invalido !',
+            #'cpf.Cpf' => 'Formato falso CPF !',
+            #'cpf.Cnpj' => 'Formato falso CNPJ !',
+            #'cpfcnpj.cpf_cnpj' => 'Formato falso do CPF ou CNPJ !',
+            #'cnh.Cnh' => 'Formato falso da sua CNH !',
+            #'renavam.Renavam' => 'Formato falso do seu  Renavam!',
+            #'placa.Placa' => 'Formato falso da sua Placa do carro !',
+            #'inscricao_estadual.inscricao_estadual:RJ' => 'Formato falso da Inscricao Estadual !',
+            #'nis.nis' => 'Formato falso do PIS/PASEP/NIT/NIS !',
+            #'cns.Cns' => 'Formato falso do seu CNS !',
+            #'certidao.Certidao' => 'Formato falso da Certidao  !',
+            #'ddd.Ddd' => 'Formato falso do DDD !',
+            #'titulo.titulo_eleitor' => 'Titulo falso !',
         ];
+
+
         $request->validate($rules, $message);
 
         //return "Cheguei no controller...";
@@ -28,3 +66,4 @@ class helperController extends Controller
 
     }
 }
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Testes de Regras de <?= utf8_encode('Validação'); ?></title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
 </head>
 <body>
     @if (session('msg_status'))
@@ -27,8 +30,61 @@
         </div>
       @endif
 
-    <input type="text" name="cpf" value="" placeholder="cpf" class="form-control" id="cpf" required />
+    <input type="text" name="cpf" value="" placeholder="cpf" class="form-control cpf" id="cpf"  />
+    <br><br>
+    <input type="text" name="cnpj" value="" placeholder="cnpj" class="form-control cnpj" id="cnpj"  />
+    <br><br>
+    <input type="text" name="cpfcnpj" value="" placeholder="cpf ou cnpj" class="form-control cpfcnpj" id="cpfcnpj"  />
+    <br><br>
+    <input type="text" name="titulo" value="" placeholder="titulo" class="form-control titulo" id="titulo"  />
+    <br><br>
+    <input type="text" name="cnh" value="" placeholder="cnh" class="form-control cnh" id="cnh"  />
+    <br><br>
+    <input type="text" name="inscricao_estadual" value="" placeholder="inscricao estadual" class="form-control inscricao_estadual" id="inscricao_estadual"  />
+    <br><br>
+    <input type="text" name="nis" value="" placeholder="PIS/PASEP/NIT/NIS" class="form-control nis" id="nis"  />
+    <br><br>
+    <input type="text" name="cns" value="" placeholder="cns" class="form-control cns" id="cns"  />
+    <br><br>
+    <input type="text" name="renavam" value="" placeholder="Renavam" class="form-control renavam" id="renavam"  />
+    <br><br>
+    <input type="text" name="placa" value="" placeholder="Placa" class="form-control" id="placa"  />
+    <br><br>
+    <input type="text" name="certidao" value="" placeholder="Certidao de nascimento/casamento/obito" class="form-control .certidao" id="certidao"  />
+    <br><br>
+    <input type="text" name="ddd" value="" placeholder="DDD" class="form-control ddd" id="ddd"  />
+    <br><br>
     <input type="submit" class="btn btn-primary" value="Enviar teste" name="enviar" >
    </form>
 </body>
 </html>
+<script>
+    $(".cpfcnpj").keydown(function(){
+    try {
+        $(".cpfcnpj").unmask();
+    } catch (e) {}
+
+    var tamanho = $(".cpfcnpj").val().length;
+
+    if(tamanho < 11){
+        $(".cpfcnpj").inputmask("999.999.999-99");
+    } else {
+        $(".cpfcnpj").inputmask("99.999.999/9999-99");
+    }
+    });
+
+    $(document).ready(function(){
+
+        $('.cpf').inputmask('999.999.999-99');
+        $('.cnpj').inputmask('99.999.999/9999-99');
+        $('.titulo').inputmask('999999999/99');
+        $('.cnh').inputmask('99999999999');
+        $('.inscricao_estadual').inputmask('99.999.99-9');
+        $('.nis').inputmask('999.99999.99-9');
+        $('.cns').inputmask('999999999999999');
+        $('.renavam').inputmask('99999999999');
+        $('.certidao').inputmask('999999999999999999999999999999-99');
+        $('.ddd').inputmask('999');
+
+    });
+</script>
